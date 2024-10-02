0328020e26674600dc280e803e6809fb19c93d8e
tagir2211 <tagir2211@gmail.com>
2024-10-02 21:42:48 +0300

    module_9_6.py

--- module_9_6.py
+++ module_9_6.py
@@ -1,11 +1,11 @@
 def all_variants(text):
     for i in range(len(text)):
         for j in range(len(text)):
-            if i+j >= (len(text)):
+            if i + j >= (len(text)):
                 continue
-            yield text[j:i+j+1]
+            yield text[j:i + j + 1]
+
 
 f = all_variants('123')
 for i in f:
     print(i)
-
